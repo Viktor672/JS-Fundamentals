0a1000a0759f23335ccb3b5bef61ca552b53741a
Viktor <149080919+Viktor672@users.noreply.github.com>
2024-06-03 17:32:13 +0300

    Delete JS Arrays Exercises directory

--- JS Arrays Exercises/04_arrayRotation.js
+++
@@ -1,8 +0,0 @@
-function solve(arr, totalRotations) {
-    for (let rotations = 1; rotations <= totalRotations; rotations++) {
-        let curElement = arr.shift();
-arr.push(curElement);
-    }
-    console.log(arr.join(" "));
-}
-solve([51, 47, 32, 61, 21], 2);
