4629773148051568712a717819372db82be98f6c
Viktor <149080919+Viktor672@users.noreply.github.com>
2024-06-21 10:15:56 +0300

    Update 03_numbers.js

--- JS Fundamentals Mid Exam Preparation -  2/03_numbers.js
+++ JS Fundamentals Mid Exam Preparation -  2/03_numbers.js
@@ -2,7 +2,6 @@ function solve(string) {
     let arr = string.split(" ");
     let sumArr = 0;
     let resultArr = [];
-    let newResultArr = [];
     let count = 0;
     for (let i = 0; i < arr.length; i++) {
         arr[i] = Number(arr[i]);
@@ -23,6 +22,7 @@ function solve(string) {
     }
     resultArr.sort((a, b) => b - a);
     if (resultArr.length > 5) {
+        let newResultArr = [];
         resultArr.sort((a, b) => b - a);
         for (let i = 0; i < resultArr.length; i++) {
             newResultArr = resultArr.slice(0, 5);
