75ab60d2aaef87e99f0de7ef9377ce25c4d68704
Viktor <149080919+Viktor672@users.noreply.github.com>
2024-06-21 10:11:14 +0300

    Update 03_man-o-war.js

--- JS Fundamentals Mid Exam Preparation - 1/03_man-o-war.js
+++ JS Fundamentals Mid Exam Preparation - 1/03_man-o-war.js
@@ -1,83 +1,35 @@
-function solve(arr) {
-    let pirateShipStatusString = arr.shift();
-    let warShipStatusString = arr.shift();
-    let maxHealth = Number(arr.shift());
-    let pirateShipStatusArr = pirateShipStatusString.split(">").map(x => Number(x));
-    let warShipStatusArr = warShipStatusString.split(">").map(x => Number(x));
- 
-    for (let i = 0; i < pirateShipStatusArr.length; i++) {
-        if (pirateShipStatusArr[i] > maxHealth) {
-            pirateShipStatusArr[i] = maxHealth;
+function solve(string) {
+    let arr = string.split(" ");
+    let sumArr = 0;
+    let resultArr = [];
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = Number(arr[i]);
+    }
+    for (let i = 0; i < arr.length; i++) {
+        sumArr += arr[i];
+    }
+    let averageSumArr = sumArr / arr.length;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > averageSumArr) {
+            resultArr.push(arr[i]);
+            count++;
         }
     }
-    for (let i = 0; i < warShipStatusArr.length; i++) {
-        if (warShipStatusArr[i] > maxHealth) {
-            warShipStatusArr[i] = maxHealth;
+    if (count === 0) {
+        console.log("No");
+        return;
+    }
+    resultArr.sort((a, b) => b - a);
+    if (resultArr.length > 5) {
+        let newResultArr = [];
+        resultArr.sort((a, b) => b - a);
+        for (let i = 0; i < resultArr.length; i++) {
+            newResultArr = resultArr.slice(0, 5);
         }
+        console.log(newResultArr.join(" "));
     }
-    let i = 0;
-    let action = arr[i];
-    while (action != "Retire") {
-        let count = 0;
-        if (action === "Status") {
-            for (let j = 0; j < pirateShipStatusArr.length; j++) {
-                if (pirateShipStatusArr[j] < 0.2 * maxHealth) {
-                    count++;
-                }
-            }
-            console.log(`${count} sections need repair.`);
-        }
-        else {
-            let tokens = action.split(" ");
-            let command = tokens.shift();
-            if (command === "Fire") {
-                let index = Number(tokens.shift());
-                let damage = Number(tokens.shift());
-                if (warShipStatusArr[index]) {
-                    warShipStatusArr[index] -= damage;
-                    if (warShipStatusArr[index] <= 0) {
-                        console.log(`You won! The enemy ship has sunken.`);
-                        return;
-                    }
-                }
-            }
-            else if (command === "Defend") {
-                let startIndex = Number(tokens.shift());
-                let endIndex = Number(tokens.shift());
-                let damage = Number(tokens.shift());
-                if (pirateShipStatusArr[startIndex] && pirateShipStatusArr[endIndex]) {
-                    for (let j = startIndex; j <= endIndex; j++) {
-                        pirateShipStatusArr[j] -= damage;
-                        if (pirateShipStatusArr[j] <= 0) {
-                            console.log(`You lost! The pirate ship has sunken.`);
-                            return;
-                        }
-                    }
-                }
-            }
-            else if (command === "Repair") {
-                let index = Number(tokens.shift());
-                let health = Number(tokens.shift());
-                if (pirateShipStatusArr[index]) {
-                    pirateShipStatusArr[index] += health;
-                    if (pirateShipStatusArr[index] > maxHealth) {
-                        pirateShipStatusArr[index] = maxHealth;
-                    }
-                }
-            }
-        }
-        i++;
-        action = arr[i];
+    else {
+        console.log(resultArr.join(" "));
     }
-    let sumPirateShip = 0;
-    let sumWarShip = 0;
-    for (let k = 0; k < pirateShipStatusArr.length; k++) {
-        sumPirateShip += pirateShipStatusArr[k];
-    }
-    for (let k = 0; k < warShipStatusArr.length; k++) {
-        sumWarShip += warShipStatusArr[k];
-    }
-    console.log(`Pirate ship status: ${sumPirateShip}`);
-    console.log(`Warship status: ${sumWarShip}`);
- 
 }
