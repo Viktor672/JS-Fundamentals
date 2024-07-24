465c6edcb813cec0e6cba04ac542e0895540d8cb
Viktor <149080919+Viktor672@users.noreply.github.com>
2024-07-24 18:05:18 +0300

    Delete Data Types and Variables directory

--- Data Types and Variables/06_gramophone.js
+++
@@ -1,8 +0,0 @@
-function solve(bandName,albumname,songName) {
-    let fullTimeForSong=(bandName.length*albumname.length)*songName.length/2;
-    let rotations=fullTimeForSong/2.5;
-    console.log(`The plate was rotated ${Math.ceil(rotations)} times.`);
-}
-solve('Black Sabbath', 'Paranoid',
-
-'War Pigs');
