02315945039756d867b7f837e61ca28ec6a603e1
Viktor <149080919+Viktor672@users.noreply.github.com>
2024-07-31 18:53:00 +0300

    Update 02_mirrorWords.js

--- 03 - Programming Fundamentals Final Exam Retake/02_mirrorWords.js
+++ 03 - Programming Fundamentals Final Exam Retake/02_mirrorWords.js
@@ -27,3 +27,4 @@ function mirrorWords(arr) {
         console.log("No mirror words!");
     }
 }
+mirrorWords(['@mix#tix3dj#poOl##loOp#wl@@bong&song%4very$long@thong#Part##traP##@@leveL@@Level@##car#rac##tu@pack@@ckap@#rr#sAw##wAs#r#@w1r']);
