1d4e8cc09fdfa647aed417ee1668792c4ccd9246
Viktor <149080919+Viktor672@users.noreply.github.com>
2024-08-03 15:46:34 +0300

    Update 09_arenaTier.js

--- JS Associative Arrays Exercise/09_arenaTier.js
+++ JS Associative Arrays Exercise/09_arenaTier.js
@@ -1,60 +1,68 @@
 function arenaTier(arr) {
-    let arenaObj = {};
-
-    let action = arr.shift();
-
-    while (action !== "Ave Cesar") {
-        if (action.includes(" -> ")) {
-            const [gladiator, technique, skill] = action.split(" -> ");
-
-            if (!arenaObj.hasOwnProperty(gladiator)) {
-                arenaObj[gladiator] = {};
+    let gladiatorObj = {};
+    let command = arr.shift();
+    while (command != "Ave Cesar") {
+        if (command.includes("->")) {
+            let tokens = command.split(" -> ");
+            let [gladiator, technique, skill] = tokens;
+            skill = Number(skill);
+            if (gladiator in gladiatorObj) {
+                if (technique in gladiatorObj[gladiator]) {
+                    if (gladiatorObj[gladiator].technique < skill) {
+                        gladiatorObj[gladiator].technique = skill;
+                    }
+                }
+                else {
+                    gladiatorObj[gladiator][technique] = skill;
+                }                                                                        //possible error
             }
-
-            if (!arenaObj[gladiator].hasOwnProperty(technique)) {
-                arenaObj[gladiator][technique] = 0;
+            else {
+                gladiatorObj[gladiator] = {};
+                gladiatorObj[gladiator][technique] = skill;
             }
-
-            arenaObj[gladiator][technique] = Math.max(arenaObj[gladiator][technique], Number(skill));
-
-        } else {
-            const [firstGlad, secondGlad] = action.split(" vs ");
-            if (arenaObj.hasOwnProperty(firstGlad) && arenaObj.hasOwnProperty(secondGlad)) {
-                let commonTech = false;
-
-                for (const technique in arenaObj[firstGlad]) {
-                    if (arenaObj[secondGlad].hasOwnProperty(technique)) {
-                        commonTech = true;
-                        if (arenaObj[firstGlad][technique] > arenaObj[secondGlad][technique]) {
-                            delete arenaObj[secondGlad];
-                        } else if (arenaObj[firstGlad][technique] < arenaObj[secondGlad][technique]) {
-                            delete arenaObj[firstGlad];
+        }
+        else {
+            let tokens = command.split(" vs ");
+            let firstGlad = tokens.shift();
+            let secondGlad = tokens.shift();
+            let hasTechniqueInCommon = false;
+            if (firstGlad in gladiatorObj && secondGlad in gladiatorObj) {
+                for (const technique in gladiatorObj[firstGlad]) {
+                    if (technique in gladiatorObj[secondGlad]) {
+                        hasTechniqueInCommon = true;
+                    }
+                    if (hasTechniqueInCommon) {
+                        if (gladiatorObj[firstGlad][technique] > gladiatorObj[secondGlad][technique]) {
+                            delete gladiatorObj[secondGlad];
+                        }
+                        else if (gladiatorObj[firstGlad][technique] < gladiatorObj[secondGlad][technique]) {
+                            delete gladiatorObj[firstGlad];
                         }
                         break;
                     }
                 }
             }
         }
-
-        action = arr.shift();
+        command = arr.shift();
     }
-
-    const entries = Object.entries(arenaObj).map(([gladiator, skills]) => {
-        const totalSkill = Object.values(skills).reduce((acc, skill) => acc + skill, 0);
-        return { gladiator, skills, totalSkill };
-    });
-
-    entries.sort((a, b) => b.totalSkill - a.totalSkill || a.gladiator.localeCompare(b.gladiator));
-
-    for (const { gladiator, skills, totalSkill } of entries) {
-        console.log(`${gladiator}: ${totalSkill} skill`);
-
-        Object.entries(skills)
-            .sort((a, b) => b[1] - a[1] || a[0].localeCompare(b[0]))
-            .forEach(([technique, skill]) => {
-                console.log(`- ${technique} <!> ${skill}`);
-            });
+    let tuples = Object.entries(gladiatorObj);
+    tuples.map(el => {
+        let totalSkill = Object.values(el[1]).reduce((acc, value) => acc + value, 0);
+        return el[2] = totalSkill;
+    })
+    tuples.sort((a, b) => b[2] - a[2] || a[0].localeCompare(b[0]));
+    for (const curEl of tuples) {
+        console.log(`${curEl[0]}: ${curEl[2]} skill`);
+        let valuesTuples = Object.entries(curEl[1]);
+        valuesTuples.sort((a, b) => b[1] - a[1] || a[0].localeCompare(b[0]));
+        for (const curTech of valuesTuples) {
+            console.log(` - ${curTech[0]} <!> ${curTech[1]}`);
+        }
     }
 }
-
-arenaTier(['Peter -> BattleCry -> 400', 'Alex -> PowerPunch -> 300', 'Stefan -> Duck -> 200', 'Stefan -> Tiger -> 250', 'Ave Cesar']);
+arenaTier([
+    'Pesho -> BattleCry -> 400',
+    'Gosho -> PowerPunch -> 300',
+    'Stamat -> Duck -> 200',
+    'Stamat -> Tiger -> 250',
+    'Ave Cesar']);
